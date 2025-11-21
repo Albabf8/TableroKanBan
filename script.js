b77a376165a3a0b928427485bb1c6d618af47a28
Albabf8 <164393689+Albabf8@users.noreply.github.com>
2025-11-21 12:44:25 +0100

    ✅New: Creación de textarea para las tarjetas

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
+const columnas = document.querySelectorAll('.columna');
 
+// Recorremos las columnas y por cada una de ellas añadimos los eventos
+columnas.forEach(columna => {
+
+    const contenedorTarjetas = columna.querySelector('.contenedorTarjetas');
+    const botonAgregarTarjeta = columna.querySelector('.botonAgregarTarjeta');
+    const contenedorNuevaTarjeta = columna.querySelector('.contenedorNuevaTarjeta');
+    const textareaTarjeta = columna.querySelector('.textareaTarjeta');
+    const botonGuardar = columna.querySelector('.botonGuardar');
+    const botonCancelar = columna.querySelector('.botonCancelar');
+
+
+    // Evento para el boton de agregar tarjeta
+    botonAgregarTarjeta.addEventListener('click', () => {
+        botonAgregarTarjeta.classList.add('hidden');
+        contenedorNuevaTarjeta.classList.remove('hidden');
+    });
+
+    // Evento para el boton de guardar tarjeta
+    botonGuardar.addEventListener('click', () => {
+        const nuevaTarjeta = document.createElement('div');
+        nuevaTarjeta.classList.add('tarjeta');
+        nuevaTarjeta.textContent = textareaTarjeta.value;
+        contenedorTarjetas.appendChild(nuevaTarjeta);
+        contenedorNuevaTarjeta.classList.add('hidden');
+    });
+
+    // Evento para el boton de cancelar
+    botonCancelar.addEventListener('click', () => {
+        contenedorNuevaTarjeta.classList.add('hidden');
+        botonAgregarTarjeta.classList.remove('hidden');
+    });
+
+
+
+
+
+
+
+});
 
 
 
